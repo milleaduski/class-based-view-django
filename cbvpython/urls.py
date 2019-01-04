dcfef1afb31cfcdb953b6ad4cfed790bb40186ba
millea <millea1994@gmail.com>
2019-01-04 22:17:39 +0700

    add authentification and url protection

--- cbvpython/urls.py
+++ cbvpython/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from .views import IndexView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tutorial/', IndexView.as_view())
+    path('tutorial/', IndexView.as_view()),
+	path('accounts/', include('django.contrib.auth.urls'))
 ]
--- cbvpython/views.py
+++ cbvpython/views.py
@@ -1,6 +1,12 @@
 from django.http import HttpResponse
 from django.views.generic import TemplateView
 
+
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
+
+@method_decorator(login_required, name='dispatch')
+
 class IndexView(TemplateView):
 	template_name = 'index.html'
 	# def get(self, request):
